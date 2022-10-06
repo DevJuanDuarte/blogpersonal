fd92b1b7abff899ce34d16aeb826ba3d13858884
DevJuanDuarte <juan.devcontact@gmail.com>
2022-10-06 15:32:00 -0500

    Cambios en apps instaled

--- blogpersonal/settings.py
+++ blogpersonal/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #MIS APPS
+    'core.apps.CoreConfig',
+    'blog.apps.BlogConfig',
+    'perfil.apps.PerfilConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
