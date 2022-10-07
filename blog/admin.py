cf0ae5b09f90b18e2532def477566f5676fa4e84
DevJuanDuarte <juan.devcontact@gmail.com>
2022-10-07 12:43:39 -0500

    Config admin django, perfil - blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+#MODELS
+from .models import Post
+
 # Register your models here.
+admin.site.register(Post)
--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+#MODELS
+from .models import Project
+
 # Register your models here.
+admin.site.register(Project)
