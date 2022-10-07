0670262c575f486aa1cc07ca215eb9d0ce819a2a
DevJuanDuarte <juan.devcontact@gmail.com>
2022-10-07 13:10:29 -0500

    Panel de administrador pt1

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,14 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+#admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'title', 'desc', 'created')
+    list_display_links = ('title',)
+    list_filter = ('created', 'updated')
+    search_fields = ('title', 'desc')
+
+    readonly_fields =('created', 'updated')
+
