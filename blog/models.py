93d6ba03d9da1aed44f5c4c4b534ceda3027b412
DevJuanDuarte <juan.devcontact@gmail.com>
2022-10-07 14:17:56 -0500

    Panel de administrador pt2

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,24 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    image = models.ImageField()
-    title = models.CharField(max_length = 200)
-    desc = models.TextField()
-    content = models.TextField()
+    image = models.ImageField(verbose_name='Imagen')
+    title = models.CharField(max_length = 200, verbose_name = 'Título')
+    desc = models.TextField(verbose_name='Descripción')
+    content = models.TextField(verbose_name='Contenido')
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(
+        auto_now_add=True, 
+        verbose_name='Fecha de creación')
+    updated = models.DateTimeField(
+        auto_now=True, 
+        verbose_name = 'Fecha de actualización')
+
+    class Meta():
+        verbose_name = 'Publicación'
+        verbose_name_plural = 'Publicaciones'
+        ordering = ['-created']
 
     def __str__(self) -> str:
         return self.title
